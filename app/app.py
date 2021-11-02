fd4094fac0a2955c8af452173f99e9926c7a06a8
Garet Rich <garet.rich@gmail.com>
2021-11-02 12:31:55 -0600

    Created heroku files

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+import os
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+
+  return render_template('index.html')
+
+@app.route('/create_card', methods=['POST'])
+def create_card():
+
+  card_num = request.form["card_num"]
+  exp_date = request.form["exp_date"]
+  cvv = request.form["cvv"]
+
+  return render_template('create.html', card_num=card_num, exp_date=exp_date, cvv=cvv)
+
+@app.route('/store_card', methods=['POST'])
+def store_card():
+  card_num = request.form["card_num"]
+  exp_date = request.form["exp_date"]
+  cvv = request.form["cvv"]
+
+  os.environ['HTTPS_PROXY'] = 'http://USERNAME:PASSWORD@tntivucxwfw.SANDBOX.verygoodproxy.com:8080'
+  response = requests.post('https://echo.apps.verygood.systems/post',
+                         json={'card_num':card_num, 'exp_date':exp_date, 'cvv':cvv},
+                         verify='path/to/sandbox.pem')
+  
+  response = response.json()
+
+  return render_template('store.html', response=response)
