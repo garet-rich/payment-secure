9227e510e7adbe7ec17d6c2e102de9d898a2d036
Garet Rich <garet.rich@gmail.com>
2021-11-03 21:35:23 -0600

    Created config

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import requests
+from app.config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -16,13 +18,6 @@ def create_card():
   exp_date = request.form["exp_date"]
   cvv = request.form["cvv"]
 
-  os.environ['HTTPS_PROXY'] = 'https://USERNAME:PASSWORD@tntivucxwfw.sandbox.verygoodproxy.com:8443'
-  res = requests.post('https://tntivucxwfw.SANDBOX.verygoodproxy.com/post',
-                         json = {'card_num':card_num, 'exp_date':exp_date, 'cvv':cvv},
-                         verify='path/to/sandbox.pem')
-  
-  res = res.json()
-
   return render_template('create.html', card_num=card_num, exp_date=exp_date, cvv=cvv)
 
 @app.route('/store_card', methods=['POST'])
@@ -30,6 +25,19 @@ def store_card():
   card_num = request.form["card_num"]
   exp_date = request.form["exp_date"]
   cvv = request.form["cvv"]
+
+  card_info = {
+    "card_num":request.form["card_num"],
+    "exp_date":request.form["exp_date"],
+    "cvv":request.form["cvv"]
+  }
+
+  os.environ['HTTPS_PROXY'] = 'https://username:password@tntivucxwfw.sandbox.verygoodproxy.com:8443'
+  res = requests.post('https://tntivucxwfw.sandbox.verygoodproxy.com/post',
+                         json = {'card_num':card_num, 'exp_date':exp_date, 'cvv':cvv},
+                         verify='path/to/sandbox.pem')
+  
+  res = res.json()
 
   return render_template('store.html', response=res)
 
